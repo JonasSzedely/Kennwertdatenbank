513d80838be227d25361dffa448f3a64d26bb910
Jonas <jszedely@gmail.com>
2026-01-12 09:17:36 +0100

    Project Start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module jonasszedely.kennwertdatenbank {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens kennwertdatenbank to javafx.fxml;
+    exports kennwertdatenbank;
+}
